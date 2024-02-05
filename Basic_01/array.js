09fd9e3f83f031ced55c4dfdefe187d4a4aca76b
surajkr5488 <surajkr5488@gmail.com>
2024-02-06 00:13:43 +0530

    first

---
+++ Basic_01/array.js
@@ -0,0 +1,28 @@
+const num = [
+  152,
+  122,
+  12,
+  122,
+  111,
+  111,
+  111,
+  11125,
+  [22, 22, 2],
+  [22322, 11, 2228, 225],
+];
+const data = num.map((datats) => {
+  if (Array.isArray(datats)) {
+    return datats.map((related) => {
+      if (Array.isArray(related)) {
+        return related.map((mapsss) => mapsss * 2);
+      } else {
+        return related * 2;
+      }
+    });
+  } else {
+    return datats;
+  }
+});
+
+console.log(data.toString());
+console.log(data.length);
