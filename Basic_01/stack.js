44a24888e64a19b8ba8b7cedad0ceae7c1a4c71c
surajkr5488 <surajkr5488@gmail.com>
2024-02-12 23:18:03 +0530

    constructor

---
+++ Basic_01/stack.js
@@ -0,0 +1,7 @@
+// *  Stack
+
+// The stack data structure is a sequential collection of elements that follows the
+// principle of Last In First Out (LIFO)
+// The last element inserted into the stack is first element to be removed
+// A stack of plates. The last plate placed on top of the stack is also the first plate
+// removed from the stack.
