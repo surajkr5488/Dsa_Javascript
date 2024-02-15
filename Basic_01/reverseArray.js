173d1507d70ffa06b5b8efed7f9e39bb20199cb3
Suraj <mukul.beyoung@gmail.com>
2024-02-15 13:33:37 +0530

    reverseArray

---
+++ Basic_01/reverseArray.js
@@ -0,0 +1,18 @@
+// const ArrayData = reverseString(data, "");
+// const String = reverseString(ArrayData, " ");
+
+// function reverseString(data, options) {
+//   return data.split(options).reverse(options).join(options);
+// }
+// console.log(String);
+
+function String(data, options) {
+  let dataArray = "";
+  for (let i = data.length - 1; i >= 0; i--) {
+    dataArray += data[i];
+  }
+  return dataArray.split(options).reverse().join(options);
+}
+const data = "hey suraj How are you";
+const dataArray = String(data, " ");
+console.log(dataArray, "fhjfhjhf");
